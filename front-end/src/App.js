f19ab84cfed4799a421f438893043d13fa62d2d0
drewmrodriguez <drewmrodriguez@gmail.com>
2021-06-23 00:01:03 -0500

    placeholder styling for Login/register, updated actions/reducer, started dashboard for app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,21 +3,33 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Login from "./components/Login";
 import Register from "./components/register";
-import { logout } from "./actions";
+// import { logout } from "./actions";
 
 class App extends Component {
 
   render() {
     return (
       <Router>
-        <Route
-          path= "/"
-          component={Login}
-        />
-        <Route
-          path= "/register"
-          component={Register}
-        />
+         <Route 
+            path = '/'
+            component = {Login}
+          />
+          <Route 
+            path = '/register'
+            component = {Register}
+          />
+          {/* <Route 
+            exact path = '/'
+            component = {Dashboard}
+          />
+          <Route 
+            path = '/profile'
+            component = {UserProfile}
+          />
+          <Route 
+            path = '/listing'
+            component = {TechForm}
+          /> */}
       </Router>
     );
   }
--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -70,3 +70,92 @@ export const fetchingUser = (id) => dispatch => {
             dispatch({ type: FETCHING_USERS_FAILURE, payload: err })
         })
 }
+
+export const FETCHING_TECH = 'FETCHING_TECH';
+export const FETCHING_TECH_SUCCESS = 'FETCHING_TECH_SUCCESS';
+export const FETCHING_TECH_FAILURE = 'FETCHING_TECH_FAILURE';
+
+export const fetchingTech = () => dispatch => {
+    dispatch({ type: FETCHING_TECH })
+    return axios
+        .get('')
+        .then(response => {
+            console.log(response.data);
+            dispatch({ type: FETCHING_TECH_SUCCESS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: FETCHING_TECH_FAILURE, payload: err })
+        })
+}
+
+
+export const ADD_TECH = 'ADD_TECH';
+export const ADD_TECH_SUCCESS = 'ADD_TECH_SUCCESS';
+export const ADD_TECH_FAILURE = 'ADD_TECH_FAILURE';
+
+export const addingItem = (tech) => dispatch => {
+    dispatch({ type: ADD_TECH })
+    axiosWithAuth()
+        .post('', tech)
+        .then(response => {
+            console.log('adding tech', response.data);
+            dispatch({ type: ADD_TECH_SUCCESS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: ADD_TECH_FAILURE, payload: err })
+        })
+}
+
+export const DELETE_TECH = 'DELETE_TECH';
+export const DELETE_TECH_SUCCESS = 'DELETE_TECH_SUCCESS';
+export const DELETE_TECH_FAILURE = 'DELETE_TECH_FAILURE';
+
+export const deleteItem = (id) => dispatch => {
+    dispatch({ type: DELETE_TECH })
+    // insert axiosWithAuth here for protected endpoints
+    axiosWithAuth()
+        .delete(``)
+        .then(response => {
+            console.log('delete action', response.data);
+            dispatch({ type: DELETE_TECH_SUCCESS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: DELETE_TECH_FAILURE, payload: err })
+        })
+}
+
+export const UPDATE_TECH = 'UPDATE_TECH';
+export const UPDATE_TECH_SUCCESS = 'UPDATE_TECH_SUCCESS';
+export const UPDATE_TECH_FAILURE = 'UPDATE_TECH_FAILURE';
+
+//tech parameter might change due to endpoints placeholder for now
+export const updateItem = (id, tech) => dispatch => {
+    dispatch({ type: UPDATE_TECH })
+    axiosWithAuth()
+        .put(``, tech)
+        .then(response => {
+            console.table("update", response.data);
+            dispatch({ type: UPDATE_TECH_SUCCESS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: UPDATE_TECH_FAILURE, payload: err })
+        })
+}
+
+// Stretch
+export const ADD_REVIEW = 'ADD_REVIEW';
+export const ADD_REVIEW_SUCCESS = 'ADD_REVIEW_SUCCESS';
+export const ADD_REVIEW_FAILURE = 'ADD_REVIEW_FAILURE'
+
+export const addingReview = (review) => dispatch => {
+    dispatch({ type: ADD_REVIEW })
+    axiosWithAuth()
+        .post('', review)
+        .then(response => {
+            console.log(response.data);
+            dispatch({ type: ADD_REVIEW_SUCCESS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: ADD_REVIEW_FAILURE, payload: err })
+        })
+}
