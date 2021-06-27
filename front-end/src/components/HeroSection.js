13fa5b9fcde9b5fa13cca300095984bc3c15f997
Chase Donovan <chase.donovan@aol.com>
2021-06-26 19:36:04 -0700

    got the homepage done

---
+++ front-end/src/components/HeroSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <h1>Use My Tech Stuff</h1>
+      <p>All your tech needs without the hastle and price of ownership.</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
