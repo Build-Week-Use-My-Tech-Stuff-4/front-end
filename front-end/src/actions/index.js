e04902823172cb7f91e39a11fa29d805293fc916
drewmrodriguez <drewmrodriguez@gmail.com>
2021-06-27 17:38:54 -0500

    final touches

--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -9,16 +9,21 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 export const login = creds => dispatch => {
-    dispatch ({type:LOGIN});
-    return axios.post('https://ptpt-use-my-tech-4.herokuapp.com/api/auth/login', creds).then(res => {
-        console.log(res)
-        localStorage.setItem('token', res.data.token);
-        dispatch({ type: LOGIN_SUCCESS, payload: res.data})
-    })
+    dispatch({ type: LOGIN })
+    let userID = null;
+    return axios
+        .post('https://ptpt-use-my-tech-4.herokuapp.com/api/auth/login', creds)
+        .then(response => {
+            console.log('login', response.data,);
+            userID = response.data.username
+            localStorage.setItem('token', response.data.token);
+            dispatch({ type: LOGIN_SUCCESS, payload: response.data })
+        })
+        .then(() => dispatch(userID))
         .catch(err => {
             dispatch({ type: LOGIN_FAILURE, payload: err })
         })
-}
+    }
 
 // Logout action
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
@@ -33,35 +38,40 @@ export const REGISTER = "REGISTER";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAILURE = "REGISTER_FAILURE";
 
-export const register = user => dispatch => {
-    dispatch ({type: REGISTER});
-    return axios.post('https://ptpt-use-my-tech-4.herokuapp.com/api/auth/register', user).then(res => {
-        console.log(res)
-        dispatch({type: REGISTER_SUCCESS, payload: res.data})
-
-    })
+export const register = creds => dispatch => {
+    dispatch({ type: REGISTER })
+    let userID = null;
+    return axios
+        .post('https://ptpt-use-my-tech-4.herokuapp.com/api/auth/register', creds)
+        .then(response => { 
+            console.log('register', response.data);
+            userID = response.data.user_id;
+            console.log('register ID', response.data.user_id);
+            dispatch({ type: REGISTER_SUCCESS, payload: response.data})
+        })
+        .then(() => dispatch(userID))
         .catch(err => {
             dispatch({ type: REGISTER_FAILURE, payload: err })
         })
 }
 
 // User actions
-// export const FETCHING_USERS = 'FETCHING_USERS';
-// export const FETCHING_USERS_SUCCESS = 'FETCHING_USERS_SUCCESS';
-// export const FETCHING_USERS_FAILURE = 'FETCHING_USERS_FAILURE';
+export const FETCHING_USERS = 'FETCHING_USERS';
+export const FETCHING_USERS_SUCCESS = 'FETCHING_USERS_SUCCESS';
+export const FETCHING_USERS_FAILURE = 'FETCHING_USERS_FAILURE';
 
-// export const fetchingUser = (id) => dispatch => {
-//     dispatch({ type: FETCHING_USERS })
-//     return axios
-//         // .get(`https://ptpt-use-my-tech-4.herokuapp.com/api/users/${id}`)
-//         .then(response => {
-//             console.log(response.data);
-//             dispatch({ type: FETCHING_USERS_SUCCESS, payload: response.data })
-//         })
-//         .catch(err => {
-//             dispatch({ type: FETCHING_USERS_FAILURE, payload: err })
-//         })
-// }
+export const fetchingUser = (id) => dispatch => {
+    dispatch({ type: FETCHING_USERS })
+    return axios
+        .get(`https://ptpt-use-my-tech-4.herokuapp.com/api/users/{user_id}`)
+        .then(response => {
+            console.log(response.data);
+            dispatch({ type: FETCHING_USERS_SUCCESS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: FETCHING_USERS_FAILURE, payload: err })
+        })
+}
 
 export const FETCHING_TECH = 'FETCHING_TECH';
 export const FETCHING_TECH_SUCCESS = 'FETCHING_TECH_SUCCESS';
--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,79 +1,69 @@
 import React from "react";
 import { connect } from "react-redux";
 import { login } from '../actions/index';
-import {Link} from 'react-router-dom';
-import { LoginContainer, InputField, Form } from '../styles/StyledComponents';
+import { LoginContainer, InputField, Btn } from '../styles/StyledComponents';
+import Loader from 'react-loader-spinner';
 
 
 class Login extends React.Component {
-  state = {
-    credentials: {
-      user_name: "",
-      password: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      credentials: {
+        username: '',
+        password: '',
+      }
     }
-  };
+  }
 
-  
-  handleChange = e => {
-    
+  handleSubmit = event => {
     this.setState({
       credentials: {
         ...this.state.credentials,
-        [e.target.name]: e.target.value
+        [event.target.name]: event.target.value
       }
-    });
+    })
   };
 
-  login = e => {
-    e.preventDefault();
-    this.props.login(this.state.credentials)
-    .then(() => this.props.history.push('/items'))
-  };
-
- 
-
-  render() {
-    return (
-      <LoginContainer>
-        <Form onSubmit={this.login}>
-            <label>
-                Username
-            
-                <InputField
-                    type="text"
-                    name="username"
-                    value={this.state.credentials.username}
-                    onChange={this.handleChange}
-                />
-            </label>
-            <label>
-                Password
-                <InputField
-                    type="password"
-                    name="password"
-                    value={this.state.credentials.password}
-                    onChange={this.handleChange}
-                />
-            </label>
-            <button>{this.props.loggingIn ? "Loading" : "Login"}</button>
-            
-
-            <Link to="/register">
-                <button>Register</button>
-            </Link>
-            
-        </Form>
-      </LoginContainer>
-    );
-  }
+  login = event => {
+    event.preventDefault();
+    this.props.login({
+      user_name: this.state.credentials.username, 
+      password: this.state.credentials.password,
+      }).then(() => {
+        this.props.history.push('/items');
+    })
 }
 
-const mapStateToProps = ({ error, loggingIn }) => ({
-    error,
-    loggingIn
-  });
-  
-  export default connect(
-    mapStateToProps,
-    { login }
-  )(Login);
+render () {
+  return (
+    <LoginContainer>
+    <h1>Please Login</h1>
+    <form onSubmit={this.login}>
+      <InputField
+        type='text'
+        name='username'
+        placeholder='Username'
+        onChange={(e) => {this.handleSubmit(e)}}
+        value={this.state.credentials.username}
+      />
+      <InputField
+        type='password'
+        name='password'
+        placeholder='Password'
+        onChange={this.handleSubmit}
+        value={this.state.credentials.password}
+      />
+     <Btn>{ this.props.loggingIn ? (<Loader type="ThreeDots" color="white" height="12" width="26" />) : ('Login')}</Btn>
+  </form>
+  </LoginContainer>
+  );
+}
+}
+
+const mapStateToProps = state => ({
+  error: state.error,
+  loggingIn: state.loggingIn
+})
+
+export default connect(mapStateToProps,{ login })(Login);
